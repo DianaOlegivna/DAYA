b7da9fecdff6b4f76e4b73195d29d60aec9dabec
Daya <fedosienkodiana14@gmail.com>
2025-08-13 22:34:58 +0300

    update

--- src/js/lottie.js
+++ src/js/lottie.js
@@ -1,12 +1,14 @@
 import lottie from 'lottie-web';
 
-export function lottieAnimation () { 
+export function lottieAnimation() { 
+   window.addEventListener('load', () => {
  lottie.loadAnimation({
   container: document.getElementById('lottie-container'),
   renderer: 'svg',
   loop: true,
   autoplay: true,
   path: "https://lottie.host/b6a80aba-7468-4437-9560-e085608b410f/IHUhdH4SjK.json"
+ });
 });
 }
    
